29c46c92fe1226ef6ce162fb2efbb54dd7f5ac20
Unknown <ignacionoguerol@gmail.com>
2017-12-07 20:47:43 +0100

    Clase lider y trabajo en clase Agente
    Hay que arreglar la linea 345 en Agente.java (la llamada a leerSensor).

--- src/helloworld/Helloworld.java
+++ src/helloworld/Helloworld.java
@@ -29,7 +29,7 @@ public class Helloworld {
        AgentsConnection.connect("isg2.ugr.es",6000,"Bellatrix","Escorpion","Russo",false);
         try {
 
-            a = new Agente(new AgentID("agentep20"));
+            a = new Agente(new AgentID("agentep21"));
             a .start();
      
         } catch (Exception ex) {
---
+++ src/helloworld/Lider.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld;
+
+/**
+ *
+ * @author nacho, ruben
+ */
+public class Lider {
+        
+    
+    /**funciones
+     * - Enviar mapa tamaño n
+     * - request accion (busca objetivo o ve a objetivo)
+     * - responder a una peticion de movimiento (PROPOSE) con AGREE/REFUSE consultando el mapa
+     * - Actualizar mapa.
+     * - consultar la memoria
+     * - inform. 
+     */
+    
+    
+
+}
