b293e7a5e73cbc92b5093098367eb30b66f814b5
Unknown <g.viddoy@gmail.com>
2017-12-12 11:05:53 +0100

    cambios memoria

---
+++ JavaApplication4/src/javaapplication4/JavaApplication4.java
@@ -0,0 +1,21 @@
+
+/**
+ *
+ * @author Grego
+ */
+public class JavaApplication4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int prueba = 5/2;
+        
+        System.out.println(prueba);
+        
+        
+    }
+    
+}
